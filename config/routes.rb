09edc0b8192159d950a3a4a748a3f69f0f47be7c
Megumi-0712 <toruansya@yahoo.co.jp>
2020-09-14 17:17:24 +0900

    Setting controller & model (categoryとcontactは除く)

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
 	root "home#top"
 	get 'about' => "home#about"
 
+#---users--
 	devise_for :users
 	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	resources :blogs, only: [:index, :new, :create, :show, :edit, :upadte, :destroy] do
+		resource :favorites, only:[:create, :destroy]
+		resources :comments, only: [:new, :create, :index, :destroy]
+	end
 end
