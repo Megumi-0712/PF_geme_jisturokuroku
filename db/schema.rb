3c7c356a98fa0b33318995b24033715212ebf896
Megumi-0712 <toruansya@yahoo.co.jp>
2020-09-25 15:51:18 +0900

    user_idとblog_idのカラム入れ忘れ

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_25_061444) do
+ActiveRecord::Schema.define(version: 2020_09_25_064526) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2020_09_25_061444) do
     t.datetime "created_at", null: false
     t.datetime "update_at"
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "categories", force: :cascade do |t|
